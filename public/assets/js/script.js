19dfeeb849f069820aef9ed2c5ef9630016d1906
Adam Brewer <adamcbrewer@gmail.com>
2012-10-29 13:49:50 +0000

    Modernizr and jQuery

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,23 @@
-(function (App) {
+(function (App, M) {
 
 	App = App || {};
 
-	//
-	// Socket.io Listeners
-	//
-	// =========================================
-	//
-	App.socket.on('broadcast', function (data) {
+	Modernizr.load({
+		load: [
+			App.siteUrl + '/assets/js/libs/jq-1.8.2.min.js'
+		],
+		complete: function () {
+			//
+			// Socket.io Listeners
+			//
+			// =========================================
+			//
+			App.socket.on('broadcast', function (data) {
 
-		console.log(data);
+				console.log(data);
 
+			});
+		}
 	});
 
-}(App));
+}(App, Modernizr));
